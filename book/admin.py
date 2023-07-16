dcb74498706991bdc9e41a8b61e41b1f81e9e58c
Mmaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2023-07-16 23:58:25 +0500

    admin models is ready

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Publisher,Author,Book ,BookImage,Genre,Language
+admin.site.register([
+    Publisher,
+    Author,
+    Book,
+    BookImage,
+    Genre,
+    Language
+])
 # Register your models here.
